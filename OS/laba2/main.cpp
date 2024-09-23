373548458ef7f451fdecb1210563465d61b261f7
artem <artem@DESKTOP-E3C3HLC>
2024-09-24 01:09:56 +0300

    add node for tree

--- OS/laba2/main.cpp
+++ OS/laba2/main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <vector>
+#include<string>
 using namespace std;
 
-int main(){
-    for(int i=0;i<10;i++){
-    cout<<"hello world"<<endl;
+class node{
+private:
+    string id;
+    node* parent;
+    vector<node*> children;
+public:
+    node(){
+        id = "undefined";
     }
-    
+
+    node(string id){
+        this->id = id;
+    }
+
+    node(node* parent){
+        this->parent = parent;
+    }
+
+    void add_child(node* child){
+        this->children.push_back(child);
+    }
+
+    string get_id(){
+        return this->id;
+    }
+
+};
+
+
+
+int main(){
+
+    node test;
+    cout<<test.get_id();
+
+
+    return 0;
 }
